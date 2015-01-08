4ac2eea4eb4fd6cc0380278dbd245f064d3153bc
Edward Opare-Aryee <ed@thisissoon.com>
2015-01-08 18:16:26 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,333 @@
+"use strict";
+
+var modRewrite = require("connect-modrewrite");
+
+module.exports = function (grunt) {
+
+    var base = grunt.option("baseDir") || "",
+        protractorConf = grunt.option("ci") ?
+                        "tests/e2e/protractor.saucelabs.conf.js" :
+                        "tests/e2e/protractor.conf.js" ;
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON("package.json"),
+
+        config: {
+            outputDir: "dist/",
+            applicationFiles: grunt.file.readJSON("scripts.json").application,
+            vendorFiles: grunt.file.readJSON("scripts.json").vendor
+        },
+
+        connect: {
+            options: {
+                hostname: "0.0.0.0",
+                port: 8000,
+                base: base
+            },
+            server: {
+                options: {
+                    livereload: true,
+                    middleware: function ( connect, options, middlewares ) {
+                        var rules = (base === "dist") ?
+                            [ "^/[^\.]*$ /index.html" ] :
+                            [ "^/app/[^\.]*$ /app/index.html" ];
+                        middlewares.unshift( modRewrite( rules ) );
+                        return middlewares;
+                    }
+                }
+            },
+            servertest: {
+                options: {
+                    keepalive: false,
+                    livereload: false,
+                    middleware: function ( connect, options, middlewares ) {
+                        var rules = [
+                            "^/app/[^\.]*$ /app/index-e2e.html"
+                        ];
+                        middlewares.unshift( modRewrite( rules ) );
+                        return middlewares;
+                    }
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                nospawn: false,
+                livereload: true
+            },
+            css: {
+                files: [
+                    "app/index.html",
+
+                    "app/less/*.less",
+                    "app/less/**/*.less",
+                    "app/less/**/**/*.less",
+
+                    "app/partials/*.html",
+                    "app/partials/**/*.html",
+                    "app/partials/**/**/*.html",
+
+                    "modules/*.html",
+                    "modules/**/*.html",
+                    "modules/**/**/*.html"
+                ],
+                tasks: ["less:development"]
+            },
+            javascript: {
+                files: [
+                    "app/js/*.js",
+                    "app/js/**/*.js",
+                    "app/js/**/**/*.js",
+
+                    "tests/unit/*.js",
+                    "tests/unit/**/*.js",
+                    "tests/unit/**/**/*.js"
+                ],
+                tasks: ["test"]
+            }
+        },
+
+        less: {
+            options: {
+                paths: ["app/less/"],
+                cleancss: false
+            },
+            development: {
+                files: { "app/css/all.css": "app/less/main.less" },
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: "app/css/all.css.map",
+                    sourceMapURL: "all.css.map",
+                    outputSourceFiles: true
+                }
+            },
+            production: {
+                files: { "<%= config.outputDir %>css/all.min.css": "app/less/main.less" },
+                options: {
+                    cleancss: true
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+            dist: {
+                src: ["<%= config.applicationFiles %>"]
+            }
+        },
+
+        jasmine: {
+            options: {
+                specs: ["tests/unit/**/*.js"],
+                keepRunner: true,
+            },
+            development: {
+                src: ["<%= config.applicationFiles %>"],
+                options: {
+                    vendor: ["<%= config.vendorFiles %>"],
+                    helpers:["app/components/angular-mocks/angular-mocks.js"],
+                    template: require("grunt-template-jasmine-istanbul"),
+                    templateOptions: {
+                        coverage: "coverage/coverage.json",
+                        report: [
+                            {
+                                type: "lcov",
+                                options: {
+                                    dir: "coverage"
+                                }
+                            },
+                            {
+                                type: "text-summary"
+                            }
+                        ]
+                    }
+                }
+            },
+            production: {
+                src: ["<%= config.outputDir %>js/app.min.js", "app/components/angular-mocks/angular-mocks.js"]
+            }
+        },
+
+        protractor: {
+            options: {
+                keepAlive: false,
+                noColor: false
+            },
+            dist: {
+                options: {
+                    configFile: protractorConf
+                }
+            }
+        },
+
+        protractor_webdriver: {
+            dist: {
+                options: {
+                    command: "webdriver-manager update && webdriver-manager start",
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                sourceMap: true,
+                separator: ";"
+            },
+            production: {
+                src: [
+                    "<%= config.vendorFiles %>",
+                    "<%= config.applicationFiles %>"
+                ],
+                dest: "<%= config.outputDir %>js/app.js"
+            }
+        },
+
+        uglify: {
+            options: {
+                sourceMap: true,
+                sourceMapIncludeSources: true,
+                enclose: {
+                    window: "window"
+                }
+            },
+            production: {
+                files: {
+                    "<%= config.outputDir %>js/app.min.js":
+                    [
+                        "<%= config.vendorFiles %>",
+                        "<%= config.applicationFiles %>"
+                    ]
+                }
+            }
+        },
+
+        copy: {
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: "app/img",
+                    src: ["**/*", "!test/**"],
+                    dest: "<%= config.outputDir %>img/"
+                }]
+            },
+            partials: {
+                files: [{
+                    expand: true,
+                    cwd: "app/partials",
+                    src: ["*.html"],
+                    dest: "<%= config.outputDir %>partials/"
+                }]
+            }
+        },
+
+        clean: {
+            beforeBuild: {
+                src: ["<%= config.outputDir %>", "docs"]
+            },
+            afterBuild: {
+                src: ["app/index-e2e.html"]
+            }
+        },
+
+        processhtml: {
+            options: {
+                strip: true
+            },
+            production: {
+                files: {
+                    "<%= config.outputDir %>index.html": ["app/index.html"]
+                }
+            },
+            e2e: {
+                options: {
+                    strip: false
+                },
+                files: {
+                    "app/index-e2e.html": ["app/index.html"]
+                }
+            }
+        },
+
+        yuidoc: {
+            compile: {
+                name: "<%= pkg.name %>",
+                description: "<%= pkg.description %>",
+                version: "<%= pkg.version %>",
+                url: "<%= pkg.homepage %>",
+                options: {
+                    paths: "app/js/",
+                    themedir: "node_modules/yuidoc-bootstrap-theme",
+                    helpers: ["node_modules/yuidoc-bootstrap-theme/helpers/helpers.js"],
+                    outdir: "docs/"
+                }
+            }
+        }
+
+
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-yuidoc");
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
+    grunt.loadNpmTasks("grunt-protractor-runner");
+    grunt.loadNpmTasks("grunt-protractor-webdriver");
+    grunt.loadNpmTasks("grunt-processhtml");
+
+    grunt.registerTask("build", [
+        "clean:beforeBuild",
+        "jshint",
+        "uglify",
+        "jasmine:production",
+        "less:production",
+        "copy",
+        "processhtml:production",
+        "yuidoc",
+        "clean:afterBuild"
+    ]);
+
+    grunt.registerTask("server", [
+        "less:development",
+        "connect:server",
+        "watch:css"
+    ]);
+
+    grunt.registerTask("serverjs", [
+        "less:development",
+        "connect:server",
+        "watch:javascript"
+    ]);
+
+    grunt.registerTask("serverall", [
+        "less:development",
+        "connect:server",
+        "watch"
+    ]);
+
+    grunt.registerTask("test", [
+        "jshint",
+        "jasmine:development"
+    ]);
+
+    grunt.registerTask("e2e", [
+        "processhtml:e2e",
+        "connect:servertest",
+        "protractor_webdriver",
+        "protractor:dist",
+        "clean:afterBuild"
+    ]);
+
+    grunt.registerTask("default", ["build"]);
+    grunt.registerTask("release", ["build"]);
+
+};
---
+++ tests/e2e/protractor.saucelabs.conf.js
@@ -0,0 +1,96 @@
+// A reference configuration file.
+exports.config = {
+
+  // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
+  // The tests will be run remotely using SauceLabs.
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  // The timeout for each script run on the browser. This should be longer
+  // than the maximum time your application needs to stabilize between tasks.
+  allScriptsTimeout: 11000,
+
+  // ----- What tests to run -----
+  //
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    'specs/*.js',
+  ],
+
+  multiCapabilities: [{
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'SOON Example (Chrome: OS X 10.10) Build: ' + process.env.TRAVIS_BUILD_NUMBER,
+    'version': '39',
+    'selenium-version': '2.43.1',
+    'platform': 'OS X 10.10'
+  }, {
+    'browserName': 'firefox',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'SOON Example (FF: OS X 10.9) Build: ' + process.env.TRAVIS_BUILD_NUMBER,
+    'version': '34',
+    'selenium-version': '2.43.1',
+    'platform': 'OS X 10.9'
+  }, {
+    'browserName': 'safari',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'SOON Example (Safari: OS X 10.10) Build: ' + process.env.TRAVIS_BUILD_NUMBER,
+    'version': '8',
+    'selenium-version': '2.43.1',
+    'platform': 'OS X 10.10'
+  }, {
+    'browserName': 'internet explorer',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'SOON Example (IE11: Win 7) Build: ' + process.env.TRAVIS_BUILD_NUMBER,
+    'version': '11',
+    'selenium-version': '2.43.1',
+    'platform': 'Windows 7'
+  }, {
+    'browserName': 'internet explorer',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'SOON Example (IE10: Win 7) Build: ' + process.env.TRAVIS_BUILD_NUMBER,
+    'version': '10',
+    'selenium-version': '2.43.1',
+    'platform': 'Windows 7'
+  }],
+
+  // ----- More information for your tests ----
+  //
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://127.0.0.1:8000',
+
+  // Selector for the element housing the angular app - this defaults to
+  // body, but is necessary if ng-app is on a descendant of <body>
+  rootElement: 'body',
+
+
+  // ----- The test framework -----
+  //
+  // Jasmine is fully supported as a test and assertion framework.
+  // Mocha has limited beta support. You will need to include your own
+  // assertion framework if working with mocha.
+  framework: 'jasmine',
+
+  // ----- Options to be passed to minijasminenode -----
+  //
+  // See the full list at https://github.com/juliemr/minijasminenode
+  jasmineNodeOpts: {
+    // onComplete will be called just before the driver quits.
+    onComplete: null,
+    // If true, display spec names.
+    isVerbose: true,
+    // If true, print colors to the terminal.
+    showColors: true,
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 30000
+  }
+
+};
