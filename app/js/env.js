ac503f3868b65f547fe079c9654733d6394062f5
James Warren <mail@jamesjwarren.com>
2015-01-20 16:22:09 +0000

    correct dev server address

--- app/js/env.js
+++ app/js/env.js
@@ -1,6 +1,6 @@
 angular.module("env", [])
 
     .constant("ENV", {
-        SERVER_ADDRESS: "http://localdocker:5000/api/",
+        SERVER_ADDRESS: "http://localdocker/api/",
         BASE_URL: "http://localhost:8000/app/"
     });
