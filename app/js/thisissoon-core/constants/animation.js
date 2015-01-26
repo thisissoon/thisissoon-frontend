3bf134ddf7917802c9b76f2704df3fa15382544b
James Warren <mail@jamesjwarren.com>
2015-01-26 09:28:20 +0000

    split constants into separate files

--- app/js/thisissoon-core/constants/animation.js
+++ app/js/thisissoon-core/constants/animation.js
@@ -2,121 +2,85 @@
 /**
  * @module thisissoon.core
  * @author SOON_
- * @class  constants
+ * @class  animation
  */
 angular.module("thisissoon.core")
-
-    /**
-     * List of sections in each view
-     * @name NAV
-     * @type {Object}
-     */
-    .constant("NAV", {
-        "home": [
-            {
-                "id": "hero",
-                "title": "Hello",
-                "navStyle": "light"
-            },{
-                "id": "project",
-                "title": "Casestudies",
-                "navStyle": "dark",
-                "icon": "icon-casestudies"
-            },{
-                "id": "services",
-                "title": "Services",
-                "navStyle": "dark"
-            },{
-                "id": "about",
-                "title": "About",
-                "navStyle": "dark"
-            },{
-                "id": "join",
-                "title": "Join",
-                "navStyle": "light"
-            },{
-                "id": "contact",
-                "title": "Contact",
-                "navStyle": "light"
-            }
-        ]
-    })
 
     .constant("SOON_LOGO", {
         "full": {
             "path,polygon,rect": [{
-                "props": { fill: "#FFFFFF" },
+                "properties": { fill: "#FFFFFF" },
                 "options": { duration: 0 }
             }],
             "#s": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0 }
                 },{
-                    "props": { opacity: 1 },
+                    "properties": { opacity: 1 },
                     "options": { duration: 0, delay: 1300 }
                 }
             ],
             "#o-1": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0 }
                 },{
-                    "props": { opacity: 1 },
+                    "properties": { opacity: 1 },
                     "options": { duration: 0, delay: 1450 }
                 }
             ],
             "#o-2": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0 }
                 },{
-                    "props": { opacity: 1 },
+                    "properties": { opacity: 1 },
                     "options": { duration: 0, delay: 1600 }
                 }
             ],
             "#n": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0 }
                 },{
-                    "props": { opacity: 1 },
+                    "properties": { opacity: 1 },
                     "options": { duration: 0, delay: 1750 }
                 }
             ],
             "#underscore": [
                 {
-                    "props": { opacity: 1, x: "0" },
+                    "properties": { opacity: 1, x: "0" },
                     "options": { duration: 0 }
                 },{
-                    "props": { opacity: 0 },
-                    "options": { duration: 0, delay: 100, loop: 4 }
+                    "properties": { opacity: 0 },
+                    "options": { duration: 0, delay: 100, loop: 5 }
                 },{
-                    "props": { x: "+=100" },
+                    "properties": { x: "+=100" },
                     "options": { duration: 0, delay: 150 }
                 },{
-                    "props": { x: "+=100" },
+                    "properties": { x: "+=100" },
                     "options": { duration: 0, delay: 150 }
                 },{
-                    "props": { x: "+=100" },
+                    "properties": { x: "+=100" },
                     "options": { duration: 0, delay: 150 }
                 },{
-                    "props": { x: "+=100" },
+                    "properties": { x: "+=100" },
                     "options": { duration: 0, delay: 150 }
                 },{
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0, delay: 300, loop: 2 }
                 }
             ]
         },
         "blinking": {
             "path,polygon,rect": [{
-                "props": { fill: "#FFFFFF" },
+                "properties": { fill: "#FFFFFF" },
                 "options": { duration: 0 }
             }],
             "#underscore": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0, delay: 300, loop: true }
                 }
             ]
@@ -124,7 +88,7 @@ angular.module("thisissoon.core")
         "underscore": {
             "#underscore": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 0, delay: 100, loop: true }
                 }
             ]
@@ -135,19 +99,19 @@ angular.module("thisissoon.core")
         "burger": {
             "#burger-top": [
                 {
-                    "props": { transformOrigin: "0%", rotateZ: "45deg", translateY: "-16px" },
+                    "properties": { transformOrigin: "0%", rotateZ: "45deg", translateY: "-16px" },
                     "options": { duration: 150 }
                 }
             ],
             "#burger-bottom": [
                 {
-                    "props": { transformOrigin: "0%", rotateZ: "-45deg", translateY: "16px" },
+                    "properties": { transformOrigin: "0%", rotateZ: "-45deg", translateY: "16px" },
                     "options": { duration: 150 }
                 }
             ],
             "#burger-middle": [
                 {
-                    "props": { opacity: 0 },
+                    "properties": { opacity: 0 },
                     "options": { duration: 50 }
                 }
             ]
