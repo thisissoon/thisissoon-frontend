8f82533f6051a6ae0e0685377e6d21e208dafb3b
James Warren <mail@jamesjwarren.com>
2015-02-06 16:28:05 +0000

    document variables in navCounter

--- app/js/thisissoon-nav/navCounter.js
+++ app/js/thisissoon-nav/navCounter.js
@@ -26,22 +26,42 @@ angular.module("thisissoon.nav.snNavCounter", [
             },
             link: function($scope, $element) {
 
+                /**
+                 * Sections from NAV config object
+                 * @property {Object} sections
+                 */
                 $scope.sections = NAV;
+
+                /**
+                 * Index of current section
+                 * @property {Number} sectionCurrent
+                 */
                 $scope.sectionCurrent = 1;
 
-                var activeLi, activeIndex, ul;
+                /**
+                 * Cached navCounter list items
+                 * @property {Object} ul
+                 */
+                var ul;
+
+                /**
+                 * Cached active navCounter list item
+                 * @property {Object} activeLi
+                 */
+                var activeLi;
 
                 /**
                  * Set nav style, active class and section index for current section
                  * @param {Object} event js event
-                 * @param {Object} data  data object { event: js event from duScroll, navStyle: sections nav style }
+                 * @param {Object} data  data object { section: id of active section, navStyle: navStyle of active section }
                  */
-                $scope.$on("snNavbar:scrollSectionChanged", function (event, data) {
-                    // set nav style and active section
+                $scope.$on("snNavbar:scrollSectionChanged", function onScrollSectionChange(event, data) {
+
+                    // set nav style and active section from data object
                     $scope.style = data.navStyle;
                     $scope.active = data.section;
 
-                    // cache nav list items
+                    // cache navCounter list items
                     ul = $element.find("ul").children();
                     ul.removeClass("active");
 
