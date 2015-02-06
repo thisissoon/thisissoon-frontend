5d56dc1991755bebf81d0bd07510e9694d31223d
James Warren <mail@jamesjwarren.com>
2015-02-06 12:40:41 +0000

    set nav counter style from scrollSectionChanged event handler

--- app/js/thisissoon-nav/navCounter.js
+++ app/js/thisissoon-nav/navCounter.js
@@ -31,15 +31,27 @@ angular.module("thisissoon.nav.snNavCounter", [
 
                 var activeNav, activeLi, activeIndex, ul;
 
+                /**
+                 * Set nav style, active class and section index for current section
+                 * @param {Object} event js event
+                 * @param {Object} data  data object { event: js event from duScroll, navStyle: sections nav style }
+                 */
                 $scope.$on("snNavbar:scrollSectionChanged", function (event, data) {
+                    // set nav style
+                    $scope.style = data.navStyle;
+
+                    // store active section href
                     activeNav = angular.element(data.event).find("a").attr("href");
 
+                    // cache nav list items
                     ul = $element.find("ul").children();
                     ul.removeClass("active");
 
+                    // set active class on active nav item
                     activeLi = angular.element($element[0].querySelector("li a[href='" + activeNav + "']")).parent();
                     activeLi.addClass("active");
 
+                    // set current section index for 'x of x' display
                     var ulNodeList = Array.prototype.slice.call(ul);
                     $scope.sectionCurrent = ulNodeList.indexOf(activeLi[0]) + 1;
                 });
