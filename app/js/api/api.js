e97141f48cbe9a0e39eeffd8eac5e554d6b468e2
James Warren <mail@jamesjwarren.com>
2015-08-25 16:57:12 +0100

    add server address to resource whitelist

--- app/js/api/api.js
+++ app/js/api/api.js
@@ -5,4 +5,28 @@
  * @module thisissoon.api
  * @author SOON_
  */
-angular.module("thisissoon.api", ["config"]);
+angular.module("thisissoon.api", [
+    "config",
+    "ngSanitize"
+])
+
+.config([
+    "$sceDelegateProvider",
+    "ENV",
+    /**
+     * @class config
+     * @param {Service} $sceDelegateProvider
+     * @param {Object}  ENV
+     */
+    function ($sceDelegateProvider, ENV) {
+
+        /**
+         * Whitelist regex for server address
+         * @property {String} serverAddress
+         */
+        var serverAddress = ENV.SERVER_ADDRESS + "**";
+
+        // add server urls to resource whitelist
+        $sceDelegateProvider.resourceUrlWhitelist([ "self", serverAddress ]);
+    }
+]);
