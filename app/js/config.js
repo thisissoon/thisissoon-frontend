81687f3415553626e641fb1d8968f41e6864548c
peeklondon <greg@peeklondon.com>
2017-03-30 12:27:09 +0100

    update address and remove phone number

--- app/js/config.js
+++ app/js/config.js
@@ -2,6 +2,6 @@ angular.module("config", [])
 
     .constant("ENV", {
         SERVER_ADDRESS: "http://localdocker/",
-        API_ADDRESS: "http://localdocker/api/",
+        API_ADDRESS: "http://thisissoon.com/api/",
         BASE_URL: "http://localhost:8000/"
     });
